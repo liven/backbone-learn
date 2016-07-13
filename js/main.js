18ca73a656025db4d6097008b5aa150feb9a1f8d
Mazepin Vasily <vasily@liven-mv.ru>
2016-07-13 20:40:10 +0300

    Валидация Моделей

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ var Person = Backbone.Model.extend({
         age: 23,
         job: 'Developer'
     },
+    validate: function (attrs) {
+
+        if (attrs.age <= 0) {
+            return 'Возраст должен быть больше 0'
+        }
+
+        if (!attrs.name) {
+            return 'Имя не должно быть пустым'
+        }
+
+    },
     walk: function () {
         return this.get('name') + ' is walking';
     }
+});
+
+//В консоли необходимо создать обьект Person и назначить невалидные данные:  person.set({'name': ''}, {validate:true})
+Person.prototype.on('invalid', function(model, error){
+    console.log(error);
 });
