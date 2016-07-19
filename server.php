96387066b98737462be7f72c49ed6e2300e2cef3
Mazepin Vasily <vasily@liven-mv.ru>
2016-07-19 20:45:31 +0300

    Изменяем модель

--- server.php
+++ server.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -16,4 +19,19 @@ $app->get('/tasks/{id}', function($id) use($app) {
     return json_encode($result);
 });
 
+$app->put('/tasks/{id}',function(Request $request, $id) use($app) {
+    $content = json_decode($request->getContent(),true);
+    $database = __DIR__.'/database/tasks.json';
+    $tasks = json_decode(file_get_contents($database), true);
+    $result = null;
+    foreach($tasks as $key => $task) {
+        if ($task['id'] == $id ) {
+            $newTask = array_merge($task, $content);
+            $tasks[$key] = $newTask;
+        }
+    }
+    file_put_contents($database, json_encode($tasks));
+    return new Response('');
+});
+$app['debug'] = true;
 $app->run();
