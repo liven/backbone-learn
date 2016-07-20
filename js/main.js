dcf84c72ff3bccac1484e617bec7985038fc168e
Mazepin Vasily <vasily@liven-mv.ru>
2016-07-20 19:18:02 +0300

    Коллекции, удаление, отрисовка, события

--- js/main.js
+++ js/main.js
@@ -15,5 +15,34 @@ $(function () {
             return "/server.php/tasks"
         }
 
+    });
+    App.Collections.Tasks = Backbone.Collection.extend({
+        model: App.Models.Task,
+        url: "/server.php/tasks"
+    });
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+        initialize: function () {
+           this.collection.on('add', this.addOne, this)
+        },
+        render: function () {
+            this.$el.empty();
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+        addOne: function(task){
+            var taskView = new App.Views.Task({model:task});
+            this.$el.append(taskView.render().el);
+        }
+    });
+    App.Views.Task = Backbone.View.extend({
+        tagName: 'li',
+        initialize: function () {
+            this.model.on('destroy', this.remove, this)
+        },
+        render: function() {
+            this.$el.html(this.model.get('title'));
+            return this;
+        }
     })
 });
--- server.php
+++ server.php
@@ -8,6 +8,12 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 
 
+$app->get('/tasks', function() use($app) {
+    $tasks = json_decode(file_get_contents(__DIR__ . '/database/tasks.json'), true);
+    return json_encode($tasks);
+});
+
+
 $app->get('/tasks/{id}', function($id) use($app) {
     $tasks = json_decode(file_get_contents(__DIR__ . '/database/tasks.json'), true);
     $result = null;
